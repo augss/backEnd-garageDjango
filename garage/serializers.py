408386d7426033ac2673c0444386009fad23b0e4
Augusto Balsanelli <augustebalsanel@proton.me>
2023-06-30 09:07:23 -0300

    Updated API Rest

--- garage/serializers.py
+++ garage/serializers.py
@@ -27,4 +27,14 @@ class CarSerializer(ModelSerializer):
         model = Car
         fields = "__all__"
 
+class CarDetailSerializer(ModelSerializer):
+    class Meta:
+        model = Car
+        fields = "__all__"
+        depth = 1
 
+
+class CarListSerializer(ModelSerializer):
+    class Meta:
+        model = Car
+        fields = ["id", "brand", "price"]
