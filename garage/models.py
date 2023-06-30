fe0625908f8477e43107db894c751ae80c3db951
Augusto Balsanelli <augustebalsanel@proton.me>
2023-06-30 08:18:21 -0300

    Created color model

--- garage/models.py
+++ garage/models.py
@@ -30,13 +30,23 @@ class Category(models.Model):
         verbose_name = "Category"
         verbose_name_plural = "Categories"
 
+class Color(models.Model):
+    description = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        return self.description
+    
+    class Meta:
+        verbose_name = "Color"
+        verbose_name_plural = "Colors"
+
 class Car(models.Model):
     model = models.CharField(max_length=50)
     year = models.IntegerField(null=True, blank=True)
-    color = models.CharField(max_length=50, null=True, blank=True)
     price = models.FloatField(null=True, blank=True)
     brand = models.ForeignKey(Brand, on_delete=models.PROTECT, related_name="cars")
     category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name="cars")
+    color = models.ForeignKey(Color, on_delete=models.PROTECT, related_name="cars")
     
     def __str__(self):
         return f"{self.brand} {self.model} {self.color} {self.year}"
