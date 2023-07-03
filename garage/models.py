5e1c8cc8925088bcf29737bb99d74452b52fd6d1
Augusto Balsanelli <augustebalsanel@proton.me>
2023-07-03 16:13:26 -0300

    Added many to many field in car model

--- garage/models.py
+++ garage/models.py
@@ -43,12 +43,11 @@ class Color(models.Model):
 class Car(models.Model):
     model = models.CharField(max_length=50)
     year = models.IntegerField(null=True, blank=True)
-    price = models.DecimalField(
-        max_digits=10, decimal_places=2, default=0, null=True, blank=True
-    )
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0, null=True, blank=True)
     brand = models.ForeignKey(Brand, on_delete=models.PROTECT, related_name="cars")
     category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name="cars")
     color = models.ForeignKey(Color, on_delete=models.PROTECT, related_name="cars")
+    accessories = models.ManyToManyField(Accessory, related_name="cars")
     
     def __str__(self):
         return f"{self.model} {self.category} {self.color} {self.year}"
@@ -56,4 +55,5 @@ class Car(models.Model):
     class Meta:
         verbose_name = "Car"
         verbose_name_plural = "Cars"
+
 
