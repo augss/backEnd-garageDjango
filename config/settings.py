be07ac59e025f9ded70eb5911afbb4048e55420b
Augusto Balsanelli <github.891d8@passfwd.com>
2023-08-18 08:29:28 -0300

    Configured uploader

--- config/settings.py
+++ config/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -40,7 +42,9 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "uploader",
     "garage",
+
 ]
 
 MIDDLEWARE = [
@@ -131,3 +135,9 @@ REST_FRAMEWORK = {
         "rest_framework.permissions.DjangoModelPermissions",
     ],
 }
+
+# App Uploader settings
+MEDIA_URL = "http://localhost:8000/media/"
+MEDIA_ENDPOINT = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
+FILE_UPLOAD_PERMISSIONS = 0o640
--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from django.urls import include, path
 
+from django.conf import settings
+from django.conf.urls.static import static
+
+from uploader.router import router as uploader_router
+
 from rest_framework.routers import DefaultRouter
 
 from garage.views import AccessoryViewSet, BrandViewSet, CarViewSet, ColorViewSet, CategoryViewSet
@@ -15,4 +20,8 @@ router.register(r"categories", CategoryViewSet)
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include(router.urls)),
+    path("api/media/", include(uploader_router.urls)),
+
 ]
+
+urlpatterns += static(settings.MEDIA_ENDPOINT, document_root=settings.MEDIA_ROOT)
