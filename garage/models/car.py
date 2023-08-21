46401a0d9dd2bdd1538949517da8ff28b3a4edfb
Augusto Balsanelli <github.891d8@passfwd.com>
2023-08-21 08:51:28 -0300

    Fixed models to satisfy assignment

--- garage/models/car.py
+++ garage/models/car.py
@@ -3,21 +3,21 @@ from django.db import models
 
 from uploader.models import Image
 
-from . import Category, Color, Accessory, Model
+from . import Category, Color, Model, Accessory
 
 class Car(models.Model):
     model = models.ForeignKey(Model, on_delete=models.PROTECT, related_name="cars")
     year = models.IntegerField(null=True, blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2, default=0, null=True, blank=True)
-    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name="cars")
     color = models.ForeignKey(Color, on_delete=models.PROTECT, related_name="cars")
     image = models.ManyToManyField(
         Image,
         related_name="+",
     )
+    accessories = models.ManyToManyField(Accessory, related_name="cars")
     
     def __str__(self):
-        return f"{self.model} {self.category} {self.color} {self.year}"
+        return f"{self.model} {self.color} {self.year}"
     
     class Meta:
         verbose_name = "Car"
